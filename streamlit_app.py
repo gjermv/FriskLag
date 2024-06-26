685e3a454bdb8d86c74bd9648d836ac02e82faf9
gjermv <gjermund.vingerhagen@gmail.com>
2024-06-27 01:16:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,20 @@
 import streamlit as st 
 import pandas as pd
+import random as rd
+
 
 players = ['Player 1','Player 2','Player 3','Player 4','Player 5','Player 6']
+st.session_state.numPlayers = 4
 
+def teamInc():
+    st.session_state.numPlayers += 1
 
-for p in players:
+st.write('Spillere')
+for p in players[:st.session_state.numPlayers]:
     st.checkbox(p)
 
+st.write('Reserver')
+for p in players[st.session_state.numPlayers:]:
+    st.checkbox(p)
+
+st.button('Bytt', on_click=teamInc)
