923ea20d5ae7352ed6992831da7c0d0ed98a8a65
gjermv <gjermund.vingerhagen@gmail.com>
2024-06-27 01:20:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,9 @@ import random as rd
 
 
 players = ['Player 1','Player 2','Player 3','Player 4','Player 5','Player 6']
-st.session_state.numPlayers = 3
+
+if 'numPlayers' not in st.session_state:
+    st.session_state.numPlayers = 3
 
 def teamInc():
     st.session_state.numPlayers += 1
