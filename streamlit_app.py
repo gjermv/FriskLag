931ae7dbe00d374e70ff48ca02ce68d609516791
gjermv <gjermund.vingerhagen@gmail.com>
2024-06-27 01:27:27 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,12 +6,19 @@ import random as rd
 players = ['Player 1','Player 2','Player 3','Player 4','Player 5','Player 6']
 
 if 'numPlayers' not in st.session_state:
-    st.session_state.numPlayers = 3
+    st.session_state.numPlayers = 4
+
+if 'scorer' not in st.session_state:
+    st.session_state.scorer = []
 
 def teamInc():
     st.session_state.numPlayers += 1
 
+def addScorer(p):
+    st.session_state.numPlayers.append('Test')
+
 st.write('Spillere')
+
 for p in players[:st.session_state.numPlayers]:
     st.checkbox(p)
 
@@ -20,4 +27,9 @@ for p in players[st.session_state.numPlayers:]:
     st.checkbox(p)
 
 st.button('Bytt', on_click=teamInc)
+st.button('Målscorer', on_click=addScorer)
 st.write('Bytt', st.session_state.numPlayers)
+
+for s in st.session_state.score:
+    st.write(s)
+    
